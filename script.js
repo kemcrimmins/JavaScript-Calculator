0adc2ae178a7e1609df479d7e5553ca528725047
Kem Crimmins <kem.crimmins@gmail.com>
2017-04-23 14:17:44 -0400

    initial javascript definitions

---
+++ script.js
@@ -0,0 +1,26 @@
+var inputValue = 0;
+var inputArray = [];
+
+function numberBuilder () {
+
+}
+
+function multiply (a, b) {
+
+}
+
+function divide (a, b) {
+
+}
+
+function calculateSum () {
+
+}
+
+function clearEntry() {
+
+}
+
+function allClear () {
+	
+}
