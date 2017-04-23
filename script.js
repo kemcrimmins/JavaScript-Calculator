63f60c82ef599993f8eeb3a30d17a478f66ffd89
Kem Crimmins <kem.crimmins@gmail.com>
2017-04-23 16:03:00 -0400

    add calculateSum function

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 var inputValue = 0;
-var inputArray = [];
+var inputArray = [1, '+',1.5,'-',20];
 
 function numberBuilder () {
 
 }
 
 function multiply (a, b) {
-
+	return a * b;
 }
 
 function divide (a, b) {
-
+	rerutn a / b;
 }
 
 function calculateSum () {
-
+	var result = 0;
+	for (var i = 0; i < inputArray.length - 1; i+=2) { // length - 1 so don't exceeed array
+		if (inputArray[i + 1] === '+') {
+			result = inputArray[i] + inputArray[i + 2];
+		} else {
+			result = inputArray[i] - inputArray[i + 2];
+		}
+		inputArray[i + 2] = result;
+	}
+	return result;
 }
 
 function clearEntry() {
-
+	return inputArray.pop();
 }
 
 function allClear () {
-	
+	inputArray = [];
 }
