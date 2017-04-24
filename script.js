eb61f6af39c29f0671fb4e22b79b8c11e6530cda
Kem Crimmins <kem.crimmins@gmail.com>
2017-04-24 11:32:32 -0400

    fix calculateSum

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-var display = document.querySelector('#dislplayArea');
+var display = document.querySelector('#displayArea');
 var displayLimit = 8;
 
 var inputValue = 0;
-var inputArray = [1, '+',1.5,'-',20]; // Reset to empty array after testing
-
-var keys = document.querySelectorAll('.key');
-for (var i = 0; i < keys.length; i++) {
-	keys[i].addEventListener('click', function() {
-		console.log(this.innerHTML);
-	});
-}
+var inputArray = []; // Reset to empty array after testing
 
 function numberBuilder () {
-
+	var number = parseFloat(display.innerHTML, 10);
+	console.log(number);
+	return number;
 }
 
 function multiply (a, b) {
@@ -23,17 +18,20 @@ function divide (a, b) {
 	return a / b;
 }
 
-function calculateSum (inputArr) { 
-	var result = 0;
-	for (var i = 0; i < inputArr.length - 1; i+=2) { // length - 1 so don't exceeed array
-		if (inputArr[i + 1] === '+') {
-			result = inputArr[i] + inputArr[i + 2];
+function calculateSum () { 
+	var input = inputArray;
+	var result = input[0];
+	for (var i = 0; i < input.length - 1; i+=2) { // length - 1 so don't exceeed array
+		if (input[i + 1] === '+') {
+			result = result + input[i + 2];
 		} else {
-			result = inputArr[i] - inputArr[i + 2];
+			result = result - input[i + 2];
 		}
-		inputArr[i + 2] = result;
+		//input[i + 2] = result;
+		console.log(result);
 	}
-	return result;
+	display.innerHTML = result;
+	inputArray = [];
 }
 
 function clearEntry() {
@@ -45,3 +43,41 @@ function allClear () {
 	display.innerHTML = '0';
 	inputArray = [];
 }
+
+/*---- addEventListener to all keys ----*/
+
+var numberKeys = document.querySelectorAll('.number');
+for (var i = 0; i < numberKeys.length; i++) {
+	numberKeys[i].addEventListener('click', function() {
+		if (display.innerHTML === '0' ||
+			display.innerHTML === '+' ||
+			display.innerHTML === '-' ||
+			display.innerHTML === 'x' ||
+			display.innerHTML === '&#247') {
+			display.innerHTML = this.innerHTML;
+		} else {
+			display.innerHTML += this.innerHTML;
+		}	
+	});
+}
+
+var operatorKeys = document.querySelectorAll('.operator');
+for (var j = 0; j < operatorKeys.length; j++) {
+	operatorKeys[j].addEventListener('click', function(){
+		if (display.innerHTML !== '+' && 
+			display.innerHTML !== '-' &&
+			display.innerHTML !== 'x' &&
+			display.innerHTML !== '&#247') 
+		{
+			inputArray.push(numberBuilder(), this.innerHTML);
+			display.innerHTML = this.innerHTML;
+		}
+	});
+}	
+
+document.querySelector('#AC').addEventListener('click', allClear);
+document.querySelector('#CE').addEventListener('click', clearEntry);
+document.querySelector('#Calculate').addEventListener('click', function(){
+	inputArray.push(numberBuilder());
+	calculateSum();
+});
